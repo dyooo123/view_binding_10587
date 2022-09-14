ceed23475e4037b0a4810a16f1e9fccfe2b4477c
aldyo21 <aldyoputra21@gmail.com>
2022-09-14 13:42:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10587/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.view_binding_10587
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.view_binding_10587.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = MainAdapter(TaskList.taskList)
+        binding?.taskRv?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/view_binding_10587/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10587
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur","Kebiasaan sehari hari untuk membuat hari semakin indah"),
+        Task("Chapter 2", "Mangan","Mangan biar ga mati"),
+        Task("Chapter 3","Ngudut","Ngudut sek biar paru paru semile"),
+        Task("Chapter 4","Gym","Biar sehat dikit"),
+        Task("CHapter 5,","Turu","TURU DEK")
+    )
+}
+
